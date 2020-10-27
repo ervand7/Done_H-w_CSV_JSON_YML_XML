d31ef52ecaf4277682fcb98e8d22f6fd383622e9
ervand7 <ervand7@mail.ru>
2020-10-27 21:05:13 +0300

    Correction of H/w

--- task_json.py
+++ task_json.py
@@ -1,29 +1,54 @@
 import json
 from collections import Counter
 import os
+from pprint import pprint
 
-file_path = os.path.join(os.getcwd(), '/Users/USER/Desktop/Open and reading file/py-homework-basic-files/3.1.formats.json.xml/newsafr.json')
+file_path = os.path.join(os.getcwd(), 'newsafr.json')
 
-def get_top_10(conditional_file_path):
+
+
+def reading_file_path(conditional_file_path):
     with open(file_path) as f:
         json_data = json.load(f)
     descriptions = json_data["rss"]["channel"]["items"]
+    pprint(descriptions)
+# reading_file_path(file_path)
 
+
+
+def split_and_filter_words(conditional_file_path, min_integer):
+    with open(file_path) as f:
+        json_data = json.load(f)
+    descriptions = json_data["rss"]["channel"]["items"]
     lst = []
     lst_2 = []
     for i in descriptions:
         lst.append(f'{i["description"]}')
-
     for i in lst:
-        for s in i.split():
-            if len(s) >= 6:
-                lst_2.append(s)
+        for word in i.split():
+            if len(word) >= min_integer:
+                lst_2.append(word)
+    pprint(lst_2)
+# split_and_filter_words(file_path, 17)
 
-    d_1 = (dict(Counter(lst_2)))
-    d_2 = sorted(d_1.items(), key=lambda x: x[1], reverse=True)
-    d_3 = ([i for i in d_2 if i[1] >= 6])
-    d_4 = d_3[:10]
-    for i in enumerate(d_4, 1):
+
+
+def split_and_filter_words(conditional_file_path, min_integer, top_integer):
+    with open(file_path) as f:
+        json_data = json.load(f)
+    descriptions = json_data["rss"]["channel"]["items"]
+    lst = []
+    lst_2 = []
+    for i in descriptions:
+        lst.append(f'{i["description"]}')
+    for i in lst:
+        for word in i.split():
+            if len(word) >= min_integer:
+                lst_2.append(word)
+    dct_with_amount = (dict(Counter(lst_2)))
+    sort_dct_with_amount = sorted(dct_with_amount.items(), key=lambda x: x[1], reverse=True)
+    lst_more_six = ([i for i in sort_dct_with_amount if i[1] >= min_integer])
+    top_10_list = lst_more_six[:top_integer]
+    for i in enumerate(top_10_list, 1):
         print(f"На {i[0]} месте находится слово '{i[1][0]}', оно встречается {i[1][1]} раз(а).")
-
-get_top_10(file_path)
+split_and_filter_words(file_path, 6, 10)
--- task_xml.py
+++ task_xml.py
@@ -1,31 +1,62 @@
 import xml.etree.ElementTree as ET
 from collections import Counter
 import os
+from pprint import pprint
 
-file_path = os.path.join(os.getcwd(), '/Users/USER/Desktop/Open and reading file/py-homework-basic-files/3.1.formats.json.xml/newsafr.xml')
+file_path = os.path.join(os.getcwd(), 'newsafr.xml')
+
+
 
 def get_top_10(conditional_file_path):
     parser = ET.XMLParser(encoding="utf-8")
     tree = ET.parse(file_path, parser)
     root = tree.getroot()
+    descriptions = root.findall("channel/item")
+    for i in descriptions:
+        pprint(i.find("description").text)
+# get_top_10(file_path)
 
+
+
+def split_and_filter_words(conditional_file_path, min_integer):
+    parser = ET.XMLParser(encoding="utf-8")
+    tree = ET.parse(file_path, parser)
+    root = tree.getroot()
     descriptions = root.findall("channel/item")
     lst = []
     lst_2 = []
     for i in descriptions:
-      i.find("description").text # а так мы получим текст
-      lst.append(i.find("description").text)
+        i.find("description").text
+        lst.append(i.find("description").text)
 
     for i in lst:
-        for s in i.split():
-            if len(s) >= 6:
-                lst_2.append(s)
+        for word in i.split():
+            if len(word) >= min_integer:
+                lst_2.append(word)
+    pprint(lst_2)
+# split_and_filter_words(file_path, 12)
 
-    d_1 = (dict(Counter(lst_2)))
-    d_2 = sorted(d_1.items(), key=lambda x: x[1], reverse=True)
-    d_3 = ([i for i in d_2 if i[1] >= 6])
-    d_4 = d_3[:10]
-    for i in enumerate(d_4, 1):
+
+
+def split_and_filter_words(conditional_file_path, min_integer, top_integer):
+    parser = ET.XMLParser(encoding="utf-8")
+    tree = ET.parse(file_path, parser)
+    root = tree.getroot()
+    descriptions = root.findall("channel/item")
+    lst = []
+    lst_2 = []
+    for i in descriptions:
+        i.find("description").text  # а так мы получим текст
+        lst.append(i.find("description").text)
+
+    for i in lst:
+        for word in i.split():
+            if len(word) >= min_integer:
+                lst_2.append(word)
+    dct_with_amount = (dict(Counter(lst_2)))
+    sort_dct_with_amount = sorted(dct_with_amount.items(), key=lambda x: x[1], reverse=True)
+    lst_more_six = ([i for i in sort_dct_with_amount if i[1] >= min_integer])
+    top_10_list = lst_more_six[:top_integer]
+    for i in enumerate(top_10_list, 1):
         print(f"На {i[0]} месте находится слово '{i[1][0]}', оно встречается {i[1][1]} раз(а).")
-
-get_top_10(file_path)
+split_and_filter_words(file_path, 6, 10)
